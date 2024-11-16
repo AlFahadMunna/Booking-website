dcf7bf16183e8e0b2aead5255b2c36c96bf78ad1
AlFahadMunna <afmunna.afm.afm@gmail.com>
2024-11-16 09:41:33 +0600

    models and routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth"
+
 
 main()
   .then(() => {
@@ -17,9 +20,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
-app.get("/api/test", async (req:Request, res: Response)=>{
-    res.json({message:"Hello from express endpoint!"})
-})
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes);
 
 app.listen(7000, ()=>{
     console.log("Server is running on localhost:7000");
